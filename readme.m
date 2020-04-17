8739708fb76ab5566e5d44022a4120a16aeb1c8c
Tom Luyten <tom.luyten@gmail.com>
2020-04-17 13:09:20 +0200

    Update readme.m

--- readme.m
+++ readme.m
@@ -1,1 +1,2 @@
-- [Workshop poster design]Berry Godec, Erik Sleijpen (https://paper.dropbox.com/published/Workshop-Poster-Design--AyM~38IX~1WrUwBLoGlffApkBg-qq4FRgMWmNddqjKBgx0yGBU)
+- [Workshop poster design](https://paper.dropbox.com/published/Workshop-Poster-Design--AyM~38IX~1WrUwBLoGlffApkBg-qq4FRgMWmNddqjKBgx0yGBU) 
+Berry Godec, Erik Sleijpen 
